2b028c9e507b7f8a4f71ffbf19597da97b617e0d
Pietro <pietro@palmesi.com>
2022-06-17 08:40:51 +0200

    Modify base url

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
-  base: "/react-pdf-highlighter/",
+  base: "/",
   build: {
     outDir: "dist",
   },
